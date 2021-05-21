debbbad61399993ad85bffce09ca8b162f1242fd
xianeml <xianemml@gmail.com>
2021-05-21 13:32:52 +0900

    [vuetify] set layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,55 +1,101 @@
 <template>
-  <div>
-    <Nuxt />
-  </div>
+  <v-app id="inspire">
+    <!-- 메뉴 바 -->
+    <v-navigation-drawer v-model="drawer" app permanent>
+      <v-list>
+        <!-- 프로필이미지 -->
+        <v-list-item>
+          <v-list-item-avatar>
+            <v-img
+              src="https://github.com/xianeml/mini-portfolio/blob/master/Img/pic.jpg?raw=true"
+            ></v-img>
+          </v-list-item-avatar>
+        </v-list-item>
+        <!-- 사용자 정보 -->
+        <v-list-item link>
+          <v-list-item-content>
+            <v-list-item-title class="title"> Mihyun </v-list-item-title>
+            <v-list-item-subtitle>@xianeml</v-list-item-subtitle>
+          </v-list-item-content>
+          <!-- 드롭다운 가능 -->
+          <v-list-item-action>
+            <v-icon>mdi-menu-down</v-icon>
+          </v-list-item-action>
+        </v-list-item>
+      </v-list>
+
+      <v-divider></v-divider>
+
+      <!-- 메뉴 리스트 -->
+      <v-list nav dense>
+        <v-list-item-group color="primary">
+          <v-list-item v-for="menu in menus" :key="menu.id">
+            <!-- 메뉴 아이콘 -->
+            <v-list-item-icon v-if="!menu.submenus"
+              ><v-icon v-text="menu.icon"></v-icon
+            ></v-list-item-icon>
+            <!-- 메뉴명-->
+            <v-list-item-content v-if="!menu.submenus">
+              <v-list-item-title v-text="menu.title"></v-list-item-title>
+            </v-list-item-content>
+
+            <!-- 서브메뉴 테스트-->
+            <v-list-group v-if="menu.submenus" :value="true" sub-group>
+              <template #activator>
+                <v-list-item-content>
+                  <v-list-item-title>{{ menu.title }}</v-list-item-title>
+                </v-list-item-content>
+              </template>
+              <v-list-item
+                v-for="[id, title, icon] in menu.submenus"
+                :key="id"
+                link
+              >
+                <v-list-item-icon>
+                  <v-icon v-text="icon"></v-icon>
+                </v-list-item-icon>
+                <v-list-item-title v-text="title"></v-list-item-title>
+              </v-list-item>
+            </v-list-group>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+
+    <!-- 상단바 -->
+    <v-app-bar app>
+      <!-- 아이콘 -->
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+      <!-- 타이틀 -->
+      <v-toolbar-title>Vuetify 연습</v-toolbar-title>
+    </v-app-bar>
+
+    <!-- 메인 영역 -->
+    <v-main> </v-main>
+  </v-app>
 </template>
 
-<style>
-html {
-  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
-    Roboto, 'Helvetica Neue', Arial, sans-serif;
-  font-size: 16px;
-  word-spacing: 1px;
-  -ms-text-size-adjust: 100%;
-  -webkit-text-size-adjust: 100%;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-font-smoothing: antialiased;
-  box-sizing: border-box;
+<script>
+export default {
+  data() {
+    return {
+      drawer: null,
+      menus: [
+        { id: 1, title: 'Dashboard', icon: 'mdi-view-dashboard' },
+        { id: 2, title: '사용자 관리', icon: 'mdi-image' },
+        { id: 3, title: '메뉴관리', icon: 'mdi-help-box' },
+        {
+          id: 4,
+          title: 'parent',
+          icon: 'mdi-help-box',
+          submenus: [
+            ['5', 'Management', 'mdi-account-multiple-outline'],
+            ['6', 'Settings', 'mdi-cog-outline'],
+          ],
+        },
+      ],
+      right: null,
+    }
+  },
 }
-
-*,
-*::before,
-*::after {
-  box-sizing: border-box;
-  margin: 0;
-}
-
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
-}
-
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
-}
-
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
-}
-
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
-}
-</style>
+</script>
--- nuxt.config.js
+++ nuxt.config.js
@@ -24,6 +24,7 @@ export default {
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
+    '@nuxtjs/vuetify',
     // https://go.nuxtjs.dev/eslint
     '@nuxtjs/eslint-module',
     // https://go.nuxtjs.dev/tailwindcss
